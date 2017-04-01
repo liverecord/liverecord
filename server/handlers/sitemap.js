c4da1146720a80a3caa6f9959a609aa1c4fc0a7b
Philipp Tkachev <zoonman@gmail.com>
2017-04-01 16:16:18 -0400

    Update sitemap

--- server/handlers/sitemap.js
+++ server/handlers/sitemap.js
@@ -3,66 +3,72 @@
  */
 
 const natural = require('natural');
-const purify = require('./purify');
-const mailer = require('./mailer');
-
-const Topic = require('../schema').Topic;
+const httpUtils = require('../common/http');
+const TopicSchema = require('../schema').Topic;
 const TOPICS_PER_PAGE = 1000;
+const errorHandler = require('./errors');
 
 function tag(tag, content) {
   return '<' + tag + '>' + content + '</' + tag + '>';
 }
 
 module.exports.router = function(req, res, next) {
-  "use strict";
-
-  var d = new Date(); // Today!
+  'use strict';
+  res.writeHead(200, {
+        'Content-Type': 'text/xml;charset: utf8'
+      }
+  );
+  res.write('<?xml version="1.0" encoding="UTF-8"?>');
+  res.write(
+      '<urlset ' +
+      'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
+  );
+  res.write(tag(
+      'url',
+          tag('loc',
+              httpUtils.url('/')
+          ) +
+          tag('lastmod', new Date().toISOString()) +
+          tag('priority', 1) +
+          tag('changefreq', 'daily')
+      )
+  );
+  let d = new Date(); // Today!
   d.setYear(d.getYear() - 1);
 
-  Topic.find({updated: {$gte: d}, spam: false, private: false, deleted: false})
+  TopicSchema
+      .find({updated: {$gte: d}, spam: false, private: false, deleted: false})
       .sort({updated: -1, created: -1})
       .limit(TOPICS_PER_PAGE)
       .select('slug category created updated')
       .populate('category')
       .lean()
-      .exec(function(err, data) {
-            if (err) {
-              handleError(err);
-            } else {
-              if (data) {
-                res.writeHead(200, {
-                      'Content-Type': 'text/xml;charset: utf8'
-                    }
-                );
-                res.write('<?xml version="1.0" encoding="UTF-8"?>');
-                res.write(
-                    '<urlset ' +
-                    'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
-                );
-                data.forEach(function(item) {
-                  if (
-                      item && item.category && item.category.slug && item.slug
-                  ) {
-                        res.write(tag(
-                            'url',
-                                tag('loc',
-                                    'https://' +
-                                    process.env.npm_package_config_server_name +
-                                    '/' +
-                                    item.category.slug + '/' +
-                                    item.slug
-                                ) +
-                                tag('lastmod', item.updated.toISOString()) +
-                                tag('priority', 0.5) +
-                                tag('changefreq', 'daily')
-                            )
-                        );
-                      }
-                });
-                res.write('</urlset>');
-                res.end();
-              }
+      .then(topics => {
+        if (topics) {
+          topics.forEach(topic => {
+            if (topic && topic.category && topic.category.slug && topic.slug) {
+              res.write(tag(
+                  'url',
+                      tag('loc',
+                          httpUtils.url(
+                              topic.category.slug + '/' +
+                              topic.slug)
+                      ) +
+                      tag('lastmod', topic.updated.toISOString()) +
+                      tag('priority', 0.5) +
+                      tag('changefreq', 'daily')
+                  )
+              );
             }
-          }
-      );
+          });
+          res.write('</urlset>');
+          res.end();
+        } else {
+          res.end();
+        }
+      })
+      .catch(err => {
+        res.end();
+        errorHandler(err);
+      });
 };
